5d5c1889584a9326fce19a09bf5f8c645412f4e1
oscar1081865 <oscar1081865@gmail.com>
2022-03-23 12:07:03 -0500

    two screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:senior_project/NewAccountPage.dart';
 
 import 'Login_Page.dart';
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: NewAccountPage(),
     );
   }
 }
